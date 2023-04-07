64c6d04e4bffc999731cce38d404057ab5550ed3
Ashik <designwithashik@gmail.com>
2023-04-07 07:23:51 +0600

    Condtional Rendering Added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
-const Cart = ({ cart, handleRemoveCart}) => {
+import './Cart.css'
+const Cart = ({ cart, handleRemoveCart }) => {
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please Add Some Products</p>
+    }
+    else {
+        message = <h1>Boroloks
+            <p>Why waste so money</p>
+        </h1>
+    }
     return (
         <div>
-            <h2>Order Summary : {cart.length}</h2>
+            {message}
+            <h2 className={cart.length >=5?'blue' : 'red'}>Order Summary : {cart.length}</h2>
+            {cart.length > 2 ? <p>Aro kino</p> : <p>Gorib</p>}
+            {cart.length === 3 && <p>Triple Tri</p>}
+            {cart.length >= 5 || <h3>5 ta to hoilo nah</h3>}
         {cart.map(tshirt =>{
             return (
             <p key={tshirt._id}>{tshirt.name}<button onClick={()=>handleRemoveCart(tshirt._id)}>X</button></p>
